660fa9f62a44e2d4a02f31958154ee6e9133787d
Harrisson Xavier <harrissonv23@gmail.com>
2025-11-16 14:46:16 -0300

    Add files via upload

---
+++ ATIVIDADE-04-JS/script.js
@@ -0,0 +1,67 @@
+//SEÇÃO 1: Operadores de Atribuição (=)
+
+var num1 = 15;
+var num2 = 7;
+var num3 = num1 + num2;
+console.log(num3);
+
+//Reatribuição de Valor:
+
+var contador = 0;
+contador = 10;
+contador = contador + 5;
+console.log(contador);
+
+
+//SEÇÃO 2: Operadores Aritméticos (+, -, *, /, %, **, ())
+
+// Operações Básicas:
+var a = 20;
+var b = 5;
+
+// soma 
+var soma = a + b;
+console.log(soma);
+
+// subtração
+var subtracao = a - b;
+console.log(subtracao);
+
+// multiplicação
+var multiplicacao = a * b;
+console.log(multiplicacao);
+
+// divisão
+var divisao = a / b;
+console.log(divisao);
+
+// operador de módulo
+var modulo = a % b;
+console.log(modulo);
+
+//Exponenciação :
+var exponenciação = 2 ** 3;
+console.log(exponenciação) ;
+
+var exponenciação = 5 ** 2;
+console.log(exponenciação) ;
+
+//Combinação de Operadores:
+
+var  x = 10, y = 4, z = 2;
+
+operadores =  (x + y) * z - (x / y);
+console.log(operadores) ;
+
+//SEÇÃO 3: Operadores de Atribuição Combinados (+=, -=, *=, /=, %=)
+
+//Soma e Atribuição (+=):
+
+var saldo = 100;
+saldo += 50; 
+console.log(saldo);
+
+var saldo = 150;
+saldo -= 30
+console.log(saldo)
+
